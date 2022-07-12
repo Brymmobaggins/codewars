651e9e0cd9e13849aeb2b2cd791bf1c69fe1f5ad
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-07-12 14:36:43 +0100

    add string repeat code challenge

---
+++ 8kyu/string-repeat.js
@@ -0,0 +1,12 @@
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples(input -> output)
+// 6, "I" -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+// my solution
+
+function repeat(number, string) {
+
+    return string.repeat(number)
+}
