376f417565173eda434a0ceee007cefd3854133a
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-07-12 14:31:31 +0100

    add string to upper case challenge

--- 8kyu/opposite-number.js
+++ 8kyu/opposite-number.js
@@ -1,5 +1,6 @@
 // Very simple, given an integer or a floating - point number, find its opposite.
 
+// My solution
 function opposite(number){
     return number > 0 ? -number : Math.abs(number)
 }
---
+++ 8kyu/uppercase.js
@@ -0,0 +1,7 @@
+// Write a function which converts the input string to uppercase
+
+// my solution
+function uppercase(string){
+
+    return string.toUpperCase()
+}
