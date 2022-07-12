1513d577ca7a4fd47875eab01771d240409ae66e
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-07-12 21:44:37 +0100

    add body mass challenge

---
+++ 8kyu/body-mass.js
@@ -0,0 +1,25 @@
+// Write function bmi that calculates body mass index(bmi = weight / height * height).
+
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+
+function bmi(weight, height){
+
+    let bmiResult = weight / ( height * height)
+    
+    if (bmiResult <= 18.5) {
+        return "Underweight"
+    } else if (bmiResult <= 25.0){
+        return "Normal"  
+    } else if ( bmiResult <= 30.0){
+        return "Overweight"
+    }else{
+        return "Obese"
+    }
+
+}
