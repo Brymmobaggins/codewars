51e435207d1f6bd0fe9b383e92d20025d089cf2b
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-07-13 04:12:24 +0100

    add string to array code challenge

---
+++ 8kyu/string-to-array.js
@@ -0,0 +1,12 @@
+// Write a function to split a string and convert it into an array of words.
+
+//  Examples(Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+// my solution
+function stringToArray(string){
+
+ return string.split(" ")
+}
