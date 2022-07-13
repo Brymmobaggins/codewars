f8f1e1d1702710bfaea747062aa35c60cb95bd0f
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-07-13 22:37:34 +0100

    add boolean to string code challenge

---
+++ 8kyu/boolean-to-string.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+function booleanToString(booleanValue){
+
+    return booleanValue.toString()
+}
+
