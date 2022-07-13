896bcea72c5fd8b5aa89e7b66008ed15d2a821a8
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-07-13 04:10:51 +0100

    add string number code challenge

---
+++ 8kyu/string-number.js
@@ -0,0 +1,9 @@
+// We need a function that can transform a string into a number.What ways of achieving this do you know ?
+
+// Note : Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+// my solution
+function converToString(string){
+    return Number(string)
+}
+
