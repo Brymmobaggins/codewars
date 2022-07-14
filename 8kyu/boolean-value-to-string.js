3a5966c74e5da052e4773ea5cd12f97ba7387d51
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-07-14 16:39:24 +0100

    add boolean value to string Yes or no challenge

---
+++ 8kyu/boolean-value-to-string.js
@@ -0,0 +1,11 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function booleanToWords(booleanValue){
+    if (booleanValue === true) {                // condition to check if boolean value is true or False
+        return "Yes"
+    }else if (booleanValue === false){
+        return "No"
+    }
+
+
+}
