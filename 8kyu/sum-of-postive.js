d322854325e8c8d13ccb02642b7f7e602483c231
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-07-14 04:14:35 +0100

    add sum of positive numbers challenge

---
+++ 8kyu/sum-of-postive.js
@@ -0,0 +1,17 @@
+// You get an array of numbers, return the sum of all of the positives ones.
+
+//  Example[1, -4, 7, 12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+function sumOfPostive(array){
+    let total = 0;
+    for (let i = 0; i < array.length; i++) {    // loop through the array
+        if(array[i] >= 0){
+            total += array[i]                  // sum the numbers greater 0 which are postive numbers
+        }
+        
+    }
+    return total                               // Return total
+}
+
