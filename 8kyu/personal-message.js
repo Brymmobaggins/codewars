5b95b759a19456909cc7f1df53334309013a2ebf
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-07-14 20:53:35 +0100

    add personal message challenge

---
+++ 8kyu/personal-message.js
@@ -0,0 +1,13 @@
+// ESCRIPTION:
+// Create a function that gives a personalized greeting.This function takes two parameters: name and owner.
+
+// Use conditionals to return the proper message:
+
+// case return
+// name equals owner	'Hello boss'
+// otherwise	'Hello guest'
+
+// my solution
+function greeting(name, owner) {
+    name === owner ? "Hello boss" : "Hello guest"
+}
