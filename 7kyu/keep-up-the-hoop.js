a1f779be9d1ef85371e6e3e583a4cde3701249df
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-07-15 23:10:22 +0100

    add keep up the hoop code challenge

---
+++ 7kyu/keep-up-the-hoop.js
@@ -0,0 +1,13 @@
+// Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him
+
+// Write a program where Alex can input(n) how many times the hoop goes round and it will return him an encouraging message:)
+
+// If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+// If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+
+// solution
+
+function hoopCount(number) {
+    //your code goes here 
+    return number >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"
+}
