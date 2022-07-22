159561e9e70210f382bfb212cc19c59cceb2fd91
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-07-22 15:03:49 +0100

    add second solution for name abbreviation code challenge

--- 7kyu/abbrevite-a-two-word-name.js
+++ 7kyu/abbrevite-a-two-word-name.js
@@ -10,7 +10,7 @@
 
 // my solution
 function abrrevName(name){
-    let nameArray = name.split(" ")                          // split the element a string
+    let nameArray = name.split(" ")                          // split the array 
     let firstName = nameArray[0]                              // grab the first string
     let lastName = nameArray[1]                              // grab the second string   
     let initials = firstName[0] + "." + lastName[0]            // concat the first name, lastname and "."
@@ -18,3 +18,10 @@ function abrrevName(name){
    
 }
 console.log(abrrevName("Ibrahim Bakare"))
+
+// my second solution 
+function abbrevName(name){
+    let words = name.split(" ")               // split the element in the array
+    return (words[0][0] + "." + words[1][0]).toUpperCase()
+}
+console.log(abbrevName('Kyle Simpson'))
