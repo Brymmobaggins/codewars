4e249cf35c266a2583e462a15bb6f92e235900be
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-07-30 13:21:07 +0100

    add second solution

--- 8kyu/string-repeat.js
+++ 8kyu/string-repeat.js
@@ -11,4 +11,14 @@ function repeat(number, string) {
     return string.repeat(number)
 }
 
+// second solution 
+function repeatString(str, num){
+    let newString  = ""
+    // loop through the number
+    for(let i = 0; i < num; i++){
+        newString += str
+    }
+    return newString
+}
+console.log(repeatString('abc', 3))   // abcabcabc
 
