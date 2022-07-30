39232da3f2fc3622638ffcbd2dcf6e4ff77b3dfe
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-07-30 19:39:09 +0100

    update comment

--- 6kyu/title-case.js
+++ 6kyu/title-case.js
@@ -8,7 +8,8 @@ function titleCase(str){
     let newString = str.split(" ")                // split every word inside
     let newArray = []                             // create new array
     for(let i = 0 ; i < newString.length; i++){   // loop through the array 
-         newArray[i]= newString[i][0].toUpperCase() + newString[i].slice(1).toLowerCase()
+        // convert the first letter inside first word to uppercase and other lower case
+         newArray[i]= newString[i][0].toUpperCase() + newString[i].slice(1).toLowerCase() 
     }
     return newArray.join(" ")                      // join the word 
 }
