85565cbdb03a00413f41e8fb703026365ec7ca31
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-07-30 19:32:55 +0100

    add title case code challenge

---
+++ 6kyu/title-case.js
@@ -0,0 +1,16 @@
+// Return the provided string with the first letter of each word capitalized.Make sure the rest of the word is in lower case.
+
+// For the purpose of this exercise, you should also capitalize connecting words like the and of.
+
+
+
+function titleCase(str){
+    let newString = str.split(" ")                // split every word inside
+    let newArray = []                             // create new array
+    for(let i = 0 ; i < newString.length; i++){   // loop through the array 
+         newArray[i]= newString[i][0].toUpperCase() + newString[i].slice(1).toLowerCase()
+    }
+    return newArray.join(" ")                      // join the word 
+}
+  
+console.log(titleCase("I'm a little tea pot"))      //  I'm A Little Tea Pot  
