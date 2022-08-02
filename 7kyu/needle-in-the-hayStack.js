3b299e5f669db3c9d641cdfc472b777553b33064
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-08-02 10:17:20 +0100

    add second solution

--- 7kyu/needle-in-the-hayStack.js
+++ 7kyu/needle-in-the-hayStack.js
@@ -6,7 +6,7 @@
 
 // Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1)
 
-
+// first solution
 function findNeedle(haystack) {
     
     for (let i = 0; i < haystack.length; i++) {             // loop through array `hayStack`
@@ -16,4 +16,11 @@ function findNeedle(haystack) {
     }
     
 }
-console.log(findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]))
+console.log(findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"])) // found needle at postion 5
+
+
+// second solution
+function findNeedle(haystack){
+    return "found needle at postion " + haystack.indexOf("needle")
+}
+console.log(findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"])) // found needle at postion 5
