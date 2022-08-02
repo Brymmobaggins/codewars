d0a1c5227d3d984c489631b8432b62f8b4a69a20
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-08-02 15:05:21 +0100

    add debugg hello world code challenge

---
+++ 7kyu/debug-say-hello.js
@@ -0,0 +1,16 @@
+// he starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output:
+
+// Hello, Mr. Spock
+
+function sayHello(name){
+    return "Hello, " + name
+}
+console.log(sayHello("Mr Bakare"))   // Hello, Mr Bakare
+
+function sayHello(name){
+    return `Hello, ${name}`
+
+} 
+console.log(sayHello("Mr daniel"))   // Hello, Mr daniel
