d9bd84e207142d5a7639f0b1d0c75f33c6c6fc71
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-08-02 10:12:50 +0100

    add findle the needle code chalenge

---
+++ 7kyu/needle-in-the-hayStack.js
@@ -0,0 +1,19 @@
+
+// Where do I Belong
+// Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+
+// For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+
+// Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1)
+
+
+function findNeedle(haystack) {
+    
+    for (let i = 0; i < haystack.length; i++) {             // loop through array `hayStack`
+        if (haystack[i] === "needle") {  // if statement to chechk if current elemnt is `needle`  
+            return "found in needle at position " + i // return `found in needle at position` and the current element position/index
+        }
+    }
+    
+}
+console.log(findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]))
