6262a1ac08085c71df28d0932412343d13f10207
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-08-07 17:00:26 +0100

    remove loop it is not needed

--- 6kyu/smallest-value-in-an-array.js
+++ 6kyu/smallest-value-in-an-array.js
@@ -7,13 +7,12 @@
 
 
 function min(arr, toReturn) {
-    for (let i = 0; i < arr.length; i++) {  // loop through the array
-        if (toReturn === 'value') {   // if statement to check if `toreturn is 'value
-            return Math.min(...arr)  // if true, return return the min value inside the array
-        } else {
-            return arr.indexOf(Math.min(...arr)) // otherwise return the index of minium value
-        }
+    if (toReturn === 'value') {   // if statement to check if `toreturn is 'value
+        return Math.min(...arr)  // if true, return return the min value inside the array
+    } else {
+        return arr.indexOf(Math.min(...arr)) // otherwise return the index of minium value
     }
+
 }
 console.log(min([100, 20, 3, 4, 5], 'value'))  // 3
 console.log(min([100, 20, 3, 4, 5], 'index'))  // 2
