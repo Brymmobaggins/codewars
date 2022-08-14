9e94dbd094fe1ce2c6be9543789ff2321931edd6
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-08-14 20:58:45 +0100

    add count sheep code challenge and soluion

---
+++ 6kyu/count-sheep.js
@@ -0,0 +1,36 @@
+// Consider an array / list of sheep where some sheep may be missing from their place.We need a function that counts the number of sheep present in the array(true means present).
+
+// function countSheep(arrayOfSheep) {
+//     let sumOfSheep = 0
+//     for (let i = 0; i < arrayOfSheep.length; i++) {
+//         if (arrayOfSheep[i] == true) {
+//             sumOfSheep += arrayOfSheep[i]
+//         }
+//     }
+//     return sumOfSheep
+// }
+// console.log(countSheep([true, true, true, false,
+//     true, true, true, true,
+//     true, false, true, false,
+//     true, false, false, true,
+//     true, true, true, true,
+//     false, false, true, true]))
+
+
+// second Solution
+
+function countSheep(arrayOfSheep) {
+    let countOfSheep = 0
+    for (let i = 0; i < arrayOfSheep.length; i++) {
+        if (arrayOfSheep[i] == true) {
+            countOfSheep++
+        }
+    }
+    return sumOfSheep
+}
+console.log(countSheep([true, true, true, false,
+    true, true, true, true,
+    true, false, true, false,
+    true, false, false, true,
+    true, true, true, true,
+    false, false, true, true]))
