eac0475243241975839cf02f7832fbeb27019d14
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-09-06 17:27:12 +0100

    add comment and fix code challenge of boolean value to string

--- 8kyu/boolean-value-to-string.js
+++ 8kyu/boolean-value-to-string.js
@@ -1,11 +1,11 @@
 // Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
 function booleanToWords(booleanValue){
-    if (booleanValue === true) {                // condition to check if boolean value is true or False
+    if (booleanValue === true) {                // condition to check if boolean Value is true
         return "Yes"
-    }else if (booleanValue === false){
+    }else if (booleanValue === false){          // or false
         return "No"
     }
 
-
 }
+console.log(booleanToWords(true))           // yes
