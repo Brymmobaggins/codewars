dc07f90123a9f18dab673b3af3007661b879d9c9
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-09-06 17:59:46 +0100

    fix code, comment oto mixed array code challenge

--- 8kyu/sum-mixed-array.js
+++ 8kyu/sum-mixed-array.js
@@ -6,8 +6,21 @@
 
 function mix(x){
     let numberSum = 0
-    for (let i = 0; i < x.length; i++) {
-        numberSum = Number(x[i]);
+    for (let i = 0; i < x.length; i++) {   // loop through the array
+        numberSum += Number(x[i]);         // convert each iteration to number, sum it and assign to numberSum variable
     }
-    return numberSum
+    return numberSum                      // return numberSum
 }
+console.log(mix([4, 5, 6, "4", "5"]))    // 24
+
+
+//  second solution
+function mix(x){
+    let numberValue = 0;
+    for(let i= 0 ;  i < x.length; i++){
+        if(Number(x[i]))
+        numberValue += Number(x[i])
+    }
+    return numberValue
+}
+console.log(mix([4, 5, 6, "4", "5"]))
---
+++ 8kyu/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+function mix(x){
+//     let numberSum = 0
+//     for (let i = 0; i < x.length; i++) {
+//         numberSum += Number(x[i]);
+//     }
+//     return numberSum
+// }
+// console.log(mix([4, 5, 6, "4", "5"]))
