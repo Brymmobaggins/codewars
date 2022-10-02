4d6fbc8a9e8a74b555d9f1702d892fe844eff188
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-10-02 20:07:23 +0100

    add exclamation code challenge

---
+++ 8kyu/exclamation.js
@@ -0,0 +1,14 @@
+// Remove an exclamation mark from the end of a string.For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+//     Examples
+// remove("Hi!") == "Hi"
+// remove("Hi!!!") == "Hi!!"
+// remove("!Hi") == "!Hi"
+// remove("!Hi!") == "!Hi"
+// remove("Hi! Hi!") == "Hi! Hi"
+
+function remove(string) {
+    return string.endsWith("!") ? string.slice(0, -1) : string
+}
+console.log(remove("Hi!")) // hi
+// console.log(remove("!Hi")) 
