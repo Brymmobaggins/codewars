9c2030c7f599872b54190dd320fe28eefac48f00
Brymmobaggins <ibro2tablet21@yahoo.com>
2023-02-19 16:50:28 +0100

    solve the take the N element code challenge

---
+++ 8kyu/take-the-first-N-element.js
@@ -0,0 +1,12 @@
+// Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+function take(array, n) {
+    let result = []   // create an empty array store our result
+    for (let i = 0; i < n; i++) {
+        if (i < n && i < array.length) {
+            result.push(array[i])
+        }
+    }
+    return result
+}
+console.log(take([10, 20, 3, 34, 51, 6, 8], 5)) // [ 1, 2, 3, 4, 5 ]
