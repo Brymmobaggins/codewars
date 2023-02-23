0141de9ce9b6b9940cffff04449206e33ff71c10
Brymmobaggins <ibro2tablet21@yahoo.com>
2023-02-23 10:29:14 +0100

    solve binary to decimal change

---
+++ 8kyu/binary-to-decimal.js
@@ -0,0 +1,8 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+
+function bintoDecimal(bin) {
+    return parseInt(bin, 2)
+}
+console.log(bintoDecimal("10001"))  // 17
+console.log(bintoDecimal("110")) // 6
