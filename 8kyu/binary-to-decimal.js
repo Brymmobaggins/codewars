060e9d63d2e7a7f068e4dccd404654a43f621ddb
Brymmobaggins <ibro2tablet21@yahoo.com>
2023-02-23 10:36:59 +0100

    update binary to decimal challenge

--- 8kyu/binary-to-decimal.js
+++ 8kyu/binary-to-decimal.js
@@ -2,6 +2,8 @@
 
 
 function bintoDecimal(bin) {
+
+    // `parseInt` takes two parameter (string, radix) `radix parameter specifies the number system to use
     return parseInt(bin, 2)
 }
 console.log(bintoDecimal("10001"))  // 17
