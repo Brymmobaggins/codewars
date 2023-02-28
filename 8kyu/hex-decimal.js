9169059f3020f105352afc59a2d0278a9575ccc1
Brymmobaggins <ibro2tablet21@yahoo.com>
2023-02-28 04:27:17 +0100

    solve hex to decimal code challenge

---
+++ 8kyu/hex-decimal.js
@@ -0,0 +1,7 @@
+// Complete the function which converts hex number(given as a string) to a decimal number.
+
+function hexToDecimal(hexToString){
+    return parseInt(hexToString, 16)
+    
+}
+console.log(hexToDecimal("baa"))
