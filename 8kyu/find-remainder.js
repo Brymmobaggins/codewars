144c14a12f831c96415e76e58377f2ed148c8d79
Brymmobaggins <ibro2tablet21@yahoo.com>
2023-04-14 17:30:27 +0100

    solve find the remainder code challenge

---
+++ 8kyu/find-remainder.js
@@ -0,0 +1,27 @@
+
+// Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+// Division by zero should return NaN.
+// Examples:
+// n = 17
+// m = 5
+// result = 2(remainder of`17 / 5`)
+
+// n = 13
+// m = 72
+// result = 7(remainder of`72 / 13`)
+
+// n = 0
+// m = -1
+// n = 0
+// m = -1
+// result = 0(remainder of`0 / -1`)
+
+// n = 0
+// m = 1
+
+function remainder(n, m){
+
+    return n > m ? n % m : m % n
+}
+console.log(remainder(2,90))
