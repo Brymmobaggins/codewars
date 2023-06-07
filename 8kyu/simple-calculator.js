d2620a9b7c8b79e4e72c65e48b78a3921aa50438
Brymmobaggins <ibro2tablet21@yahoo.com>
2023-06-07 07:52:35 +0100

    solve simple calcualator code challenge

---
+++ 8kyu/simple-calculator.js
@@ -0,0 +1,39 @@
+/*You are required to create a simple calculator that returns the result of addition, subtraction, multiplication or division of two numbers.
+
+Your function will accept three arguments:
+The first and second argument should be numbers.
+The third argument should represent a sign indicating the operation to perform on these two numbers.
+
+if the variables are not numbers or the sign does not belong to the list above a message "unknown value" must be returned.*/
+function calculator(a, b, sign) {
+    if (typeof (a) !== 'number' || typeof (b) !== 'number' || (sign !== '+' && sign !== '-' && sign !== '*' && sign !== '/')) {
+        return 'unknown value';
+    }
+    switch (sign) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return a / b;
+        default:
+            return 'unknown value';
+    }
+}
+
+// solution 2
+
+function calculator(a, b, sign) {
+    switch (sign) {
+        case "+":
+            return a + b
+        case "-":
+            return a - b
+        case "/":
+            return a / b;
+        default:
+            return "unknown  value"
+    }
+}
