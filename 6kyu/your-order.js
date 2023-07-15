e56d73c211477559279ce0d92b4eb1a912f97f79
Brymmobaggins <ibro2tablet21@yahoo.com>
2023-07-16 00:59:49 +0100

    solve your order code challenge

---
+++ 6kyu/your-order.js
@@ -0,0 +1,26 @@
+/*Your task is to sort a given string.Each word in the string will contain a single number.This number is the position the word should have in the result.
+
+    Note: Numbers can be from 1 to 9. So 1 will be the first word(not 0).
+
+If the input string is empty, return an empty string.The words in the input String will only contain valid consecutive numbers.
+
+Examples
+
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -- > "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -- > "" **/
+
+function order(words) {
+    let arr = words.split(" "); // spliting into array of individual strings
+    let sortedArray = []        
+    for (let i = 0; i <= arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j].indexOf(i) >= 0) {
+                sortedArray.push(arr[j])
+            }
+        }
+    }
+    return sortedArray.join(' ')
+
+}
+console.log(order("is2 Thi1s T4est 3a"))
