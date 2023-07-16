1db013e4d91e0ea36a001515e0dcb91440a78f92
Brymmobaggins <ibro2tablet21@yahoo.com>
2023-07-16 01:22:35 +0100

    solve apple turnover code challenge

---
+++ 8kyu/apple-turnOver.js
@@ -0,0 +1,7 @@
+/*Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.*/
+
+function apple(x) {
+
+    return x * x > 1000 ? "It\'s hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox"
+}
+console.log(apple(10000))
