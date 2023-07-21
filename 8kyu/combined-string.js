01752484b0b768a6f4075650751a1ef9db9933f8
Brymmobaggins <ibro2tablet21@yahoo.com>
2023-07-21 20:57:24 +0100

    solve combined string challenge

---
+++ 8kyu/combined-string.js
@@ -0,0 +1,11 @@
+/*Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+
+combineNames('James', 'Stevens')u*/
+
+function combineNames(first, last){
+    
+    return first + " " + last
+}
+console.log(combineNames('James', 'Stevens'))
