3904f754e5df15a21b2437fb84e3457d89986d92
brymmobaggins <ibro2tablet21@yahoo.com>
2023-12-29 16:12:01 +0100

    solve code challenge

---
+++ 6kyu/count-the-smile-face.js
@@ -0,0 +1,60 @@
+/*DESCRIPTION:
+Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+Rules for a smiling face:
+
+Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+Every smiling face must have a smiling mouth that should be marked with either ) or D
+No additional characters are allowed except for those mentioned.
+
+Valid smiley face examples: :) :D ;-D :~)
+Invalid smiley faces: ;( :> :} :]
+
+Example
+countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+Note
+In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same*/
+
+
+/**
+ * Counts the number of valid smiley faces in an array.
+ *
+ * A valid smiley face must have a valid pair of eyes (':' or ';') and a valid mouth (')' or 'D').
+ * A valid smiley face may optionally have a nose ('-').
+ *
+ * @param {string[]} arr - The array of strings to check.
+ * @returns {number} The number of valid smiley faces in the array.
+ */
+function countSmileys(arr) {
+    let smileyFaces = [
+        ":)",
+        ";)",
+        ":-)",
+        ";-)",
+        ";~)",
+        ":~)",
+        ":D",
+        ";D",
+        ":-D",
+        ":~D",
+        ";-D",
+        ";~D",
+    ];
+    let count = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < smileyFaces.length; j++) {
+            if (arr[i] === smileyFaces[j]) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+
+
+console.log(countSmileys([';D', ':-(', ':-)', ';~)']))
