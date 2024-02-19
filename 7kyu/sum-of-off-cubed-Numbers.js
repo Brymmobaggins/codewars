252d65d310169a914bc09752e0c5039c04ce158e
brymmobaggins <ibro2tablet21@yahoo.com>
2024-02-19 09:11:41 +0100

    add doc

--- 7kyu/sum-of-off-cubed-Numbers.js
+++ 7kyu/sum-of-off-cubed-Numbers.js
@@ -2,18 +2,19 @@
 Find the sum of the odd numbers within an array, after cubing the initial integers. The function should return undefined if any of the values aren't numbers.*/
 
 function cubeOdd(arr) {
-    let oddSum = 0
-    for (let i = 0; i < arr.length; i++) {
-        if (isNaN(arr[i])) {
+    let oddSum = 0         // create a variable to hold the sum
+    for (let i = 0; i < arr.length; i++) { // loop through the arr
+        if (isNaN(arr[i])) {        //  if any of the values arent number return undefined
             return undefined
         }
-        let cubed = Math.pow(arr[i], 3)
-        if (cubed % 2 != 0) {
-            oddSum += cubed
+        let cubed = Math.pow(arr[i], 3)  // cubed the value inside the arr
+        if (cubed % 2 != 0) {            // if cubed value is odd
+            oddSum += cubed              // sum all odd values
         }
     }
     return oddSum
 }
-console.log(cubeOdd([-3, -2, 2, 3]))
-console.log(cubeOdd([1, 2, 3, 4]))
+console.log(cubeOdd([-3, -2, 2, 3])) // 0
+console.log(cubeOdd([1, 2, 3, 4])) // 28
+console.log(cubeOdd([1, "b", 3, 4])) // undefined
 
