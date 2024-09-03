1bfb2842efe8cc4051667e8be4103b085266963a
brymmobaggins <ibro2tablet21@yahoo.com>
2024-09-03 02:37:27 +0100

    photocopier

---
+++ 7kyu/office/office-photocoier.js
@@ -0,0 +1,22 @@
+/**
+ * The bloody photocopier is broken... Just as you were sneaking around the office to print off your favourite binary code!
+ *
+ * Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
+ *
+ * Given a string of binary, return the version the photocopier gives you as a string.
+ *
+ * @format
+ */
+
+function broken(string) {
+  let res = "";
+  for (let x of string) {
+    if (x === "0") {
+      res += "1";
+    } else {
+      res += "0";
+    }
+  }
+  return res;
+}
+console.log(broken("0011"));
