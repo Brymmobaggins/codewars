7e1f852305cf0f7e0a0b106432352efe1ab89810
brymmobaggins <ibro2tablet21@yahoo.com>
2025-06-22 20:46:27 +0100

    solve vacate transportation

---
+++ 8kyu/vacation-transport.js
@@ -0,0 +1,43 @@
+/**
+ * After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
+ *
+ * You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+ *
+ * Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+ *
+ * Write a code that gives out the total amount for different days(d).
+ *
+ * @format
+ */
+
+// solution i
+function rentalCost(d) {
+  let total = d * 40;
+  if (d >= 7) {
+    return total - 50;
+  } else if (d >= 3) {
+    return total - 20;
+  }
+  return total;
+}
+console.log(rentalCost(3));
+
+
+//  solution ii
+function baseCost(days, rate) {
+  return days * rate;
+}
+
+function discountRate(days) {
+  if (days >= 7) {
+    return 50;
+  } else if (days >= 3) {
+    return 20;
+  } else {
+    return 0;
+  }
+}
+
+function rentalCarCost(days) {
+  return baseCost(days, 40) - discountRate(days);
+}
